05ad4006fdd0dc871df099507908c9eef32eec70
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-04-17 17:17:47 +0530

    mobile device full scan css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Scanner from "./components/version4/Scanner";
 const App = () => {
   return (
     <div className="App">
-      <div className="container-sm border border-primary rounded p-2 bg-body-tertiary">
+      <div className="container-sm border border-primary rounded p-1">
         <div className="row">
         
             {/* <QrCodeScanner /> */}
--- src/components/version4/Scanner.jsx
+++ src/components/version4/Scanner.jsx
@@ -6,20 +6,53 @@ const Scanner = () => {
       console.log(result.getText());
     },
   });
-  const videoStyle = {
-    transform: "scale(1.5)", // Increase scale to make code appear larger
-    transformOrigin: "center", // Keep aspect ratio
-    objectFit: "cover", // Crop video to focus on center
-  };
+  const isMobileDevice = /Mobi/.test(navigator.userAgent);
+ /// added css for zoom in feature support
+ const scannerContainerStyle = {
+  position: "relative",
+  width: "100vw",
+  height: isMobileDevice ? "100vh" : "auto",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "black",
+  maxWidth: "600px",
+  margin: "0 auto",
+};
+
+const scannerTargetStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  height: "80%",
+  border: "2px dashed white",
+  borderRadius: "10px",
+  boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+  zIndex: "1",
+};
+
+const videoStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%) scale(1.5)", // Increase scale to make code appear larger
+  transformOrigin: "center", // Keep aspect ratio
+  objectFit: "cover", // Crop video to focus on center
+  zIndex: "0",
+  maxWidth: "100%",
+  height: "auto",
+};  
 
   return (
-    <>
-      <div className="scanner-container">
-        <div className="scanner-target" />
+    <div style={scannerContainerStyle}>
+      <div style={scannerTargetStyle}></div>
+      <div className="video-container">
         <video style={videoStyle} className="scanner" ref={ref} />
       </div>
       {scannedResult && <p>Scanned result: {scannedResult}</p>}
-    </>
+    </div>
   );
 };
 
