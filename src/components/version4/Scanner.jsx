429480e196d061edd220eb535a40c052cbf01773
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-04-17 16:51:14 +0530

    css version 2 added

--- src/components/version4/Scanner.jsx
+++ src/components/version4/Scanner.jsx
@@ -6,12 +6,17 @@ const Scanner = () => {
       console.log(result.getText());
     },
   });
+  const videoStyle = {
+    transform: "scale(1.5)", // Increase scale to make code appear larger
+    transformOrigin: "center", // Keep aspect ratio
+    objectFit: "cover", // Crop video to focus on center
+  };
 
   return (
     <>
       <div className="scanner-container">
-        <video className="scanner" ref={ref} />
         <div className="scanner-target" />
+        <video style={videoStyle} className="scanner" ref={ref} />
       </div>
       {scannedResult && <p>Scanned result: {scannedResult}</p>}
     </>
