97352a089850c85a973d9c7ae29a188f49f38432
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-04-17 18:36:14 +0530

    added "transform: scale(2.5)", to zoom video elm

--- src/components/version4/Scanner.jsx
+++ src/components/version4/Scanner.jsx
@@ -1,32 +1,19 @@
 import useZxing from "./useZxing";
-import "./Scanner.css"
+import "./Scanner.css";
 const Scanner = () => {
   const { ref, scannedResult } = useZxing({
     onResult: (result) => {
       console.log(result.getText());
     },
   });
-const videoStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%) scale(1.5)", // Increase scale to make code appear larger
-  transformOrigin: "center", // Keep aspect ratio
-  objectFit: "cover", // Crop video to focus on center
-  zIndex: "0",
-  maxWidth: "100%",
-  height: "auto",
-};  
-
   return (
     <>
-
-    <div className="scanner-container">
-      <video className="scanner" ref={ref}/>
-      <div className="scanner-target" />
-    </div>
-    {scannedResult && <p>Scanned result: {scannedResult}</p>}
-  </>
+      <div className="scanner-container">
+        <video className="scanner" ref={ref} />
+        <div className="scanner-target" />
+      </div>
+      {scannedResult && <p>Scanned result: {scannedResult}</p>}
+    </>
   );
 };
 
